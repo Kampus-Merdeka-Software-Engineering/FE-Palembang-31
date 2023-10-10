32b78e26d9c83c3fd43c83405cc58b19b4fc41f7
fahmiiabdullah <fahmiabdullah020@gmail.com>
2023-10-10 20:53:14 +0700

    delete

--- Capstone Project Grup 31/js/navbar.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.menu-navbar');
-
-menu.onclick = () => {
-  menu.classList.toggle('bx-x');
-  navbar.classList.toggle('open');
-}
